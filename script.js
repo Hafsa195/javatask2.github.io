feec114b80203776670bca8f0a885da2453740e0
Hafsa195 <hafsachouhan@gmail.com>
2024-09-30 23:25:26 -0700

    task2

---
+++ script.js
@@ -0,0 +1,11 @@
+function isEvenOrOdd(num) {
+    if (num % 2 === 0) {
+      console.log(`${num} is even.`);
+    } else {
+      console.log(`${num} is odd.`);
+    }
+  }
+  
+  // Example usage:
+  isEvenOrOdd(10); // Output: 10 is even.
+  isEvenOrOdd(11); // Output: 11 is odd.
